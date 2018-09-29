a476da2604573104d180caf1ed8596ed303fa3c5
Gustavo Neto <gustavosantosneto@gmail.com>
2018-09-29 16:25:51 -0300

    estruturando

---
+++ Jack Flag/Assets/Scripts/CenarioCreator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenarioCreator : MonoBehaviour
+{
+
+    public int columns = 32;
+    public int rows = 32;
+    public GameObject floorTile;
+    private Transform boardHolder;
+
+    public Char[] players;
+
+    void Awake()
+    {
+        MakeFloor();
+        CreatePlayers();
+    }
+
+
+    void CreatePlayers()
+    {
+        for (int i = 0, len = players.Length; i < len; i++)
+        {
+
+            Instantiate(players[i], new Vector3(players[i].startX, 0, players[i].startY), Quaternion.identity);
+        }
+
+    }
+
+    void MakeFloor()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        for (int x = 0; x < columns; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                GameObject instance = Instantiate(floorTile, new Vector3(x, -0.1f, y), Quaternion.identity) as GameObject;
+
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
---
+++ Jack Flag/Assets/Scripts/Char.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Char : MonoBehaviour
+{
+    public float startX = 1;
+    public float startY = 1;
+
+    private Rigidbody _rb;
+    private bool _isSelected = false;
+
+    private const int _moveStepLength = 1;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Camera.allCameras[0] == null)
+                return;
+
+            Ray ray = Camera.allCameras[0].ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                var clickedGameObject = hit.transform.gameObject;
+
+                if (clickedGameObject.tag == "Floor")
+                    _GotoClickedTile(clickedGameObject);
+                else if (clickedGameObject.tag == "Player")
+                {
+
+                    _isSelected = true;
+                }
+            }
+
+        }
+
+    }
+
+    private void _GotoClickedTile(GameObject tile)
+    {
+        var destinePosition = tile.transform.position;
+
+        _rb.MovePosition(destinePosition);
+    }
+}
