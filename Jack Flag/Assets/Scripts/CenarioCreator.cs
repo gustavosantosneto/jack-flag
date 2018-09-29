8063eed73f24c96a0cd4083c7dbf8d35a3a6c766
Eduardo <eduardopoleze888@gmail.com>
2018-09-29 19:38:18 -0300

    Salve

--- Jack Flag/Assets/Scripts/CenarioCreator.cs
+++ Jack Flag/Assets/Scripts/CenarioCreator.cs
@@ -13,8 +13,10 @@ public class CenarioCreator : MonoBehaviour
 
     public GameObject[] concretoTiles;
     public GameObject[] aguaTiles;
-
     public GameObject[] grassTiles;
+
+    public GameObject[] flag_black;
+    public GameObject[] flag_white;
 
     public Char[] players;
 
@@ -38,6 +40,7 @@ public class CenarioCreator : MonoBehaviour
     void MakeFloor()
     {
         _boardHolder = new GameObject("Board").transform;
+        GameObject instance = null;
 
         for (int x = 0; x < rows; x++)
         {
@@ -46,35 +49,50 @@ public class CenarioCreator : MonoBehaviour
 
                 GameObject toInstantiate = grassTiles[0];
                 int aux = 7;
+
                 int[] centroAgua1 = new int[] { 23, 8 };
                 int raioAgua1 = 5;
+
+                int[] centroAgua2 = new int[] { 9, 24 };
+                int raioAgua2 = 5;
 
                 int[] centroArvore1 = new int[] { 10, 13 };
                 int[] centroArvore11 = new int[] { 15, 19 };
                 int raioArvore1 = 6;
                 int raioArvore11 = 10;
 
+                int[] centroArvore2 = new int[] { 22, 19 };
+                int[] centroArvore22 = new int[] { 17, 13 };
+                int raioArvore2 = 6;
+                int raioArvore22 = 10;
+                
+
                 int[] pos = new int[] { x, y };
+                               
 
                 if ((x + y < aux) || ((rows - x - 1) + (rows - y - 1) < aux))
                 {
                     toInstantiate = concretoTiles[0];
                 }
-                else if (InsideCircle(centroAgua1, raioAgua1, pos))
+                else if (InsideCircle(centroAgua1, raioAgua1, pos) || (InsideCircle(centroAgua2, raioAgua2, pos)))
                 {
                     toInstantiate = aguaTiles[0];
                 }
-                else if (InsideCircle(centroArvore1, raioArvore1, pos) && !InsideCircle(centroArvore11, raioArvore11, pos))
+                else if ((InsideCircle(centroArvore1, raioArvore1, pos) && !InsideCircle(centroArvore11, raioArvore11, pos)) || (InsideCircle(centroArvore2, raioArvore2, pos) && !InsideCircle(centroArvore22, raioArvore22, pos)))
                 {
                     toInstantiate = aguaTiles[0];
                 }
 
-                GameObject instance = Instantiate(toInstantiate, new Vector3(x, 0f, y), Quaternion.identity) as GameObject;
-
+                instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
                 instance.transform.SetParent(_boardHolder);
             }
+            
         }
 
+        instance = Instantiate(flag_black[0], new Vector3(2, 2, 0f), Quaternion.identity) as GameObject;
+        instance.transform.SetParent(_boardHolder);
+        instance = Instantiate(flag_white[0], new Vector3(29, 29, 0f), Quaternion.identity) as GameObject;
+        instance.transform.SetParent(_boardHolder);
     }
 
     bool InsideCircle(int[] center, int radius, int[] position)
