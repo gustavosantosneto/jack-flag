69186a258ffc020e75cf35c0db12fe957345b639
Gustavo Neto <gustavosantosneto@gmail.com>
2018-09-30 01:07:16 -0300

    Delete unused packages;Char walk feature

--- Jack Flag/Assets/Scripts/CenarioCreator.cs
+++ Jack Flag/Assets/Scripts/CenarioCreator.cs
@@ -1,47 +1,57 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CenarioCreator : MonoBehaviour
 {
     private Transform _boardHolder;
 
-    public int columns = 32;
-    public int rows = 32;
-
+    public static int columns = 32;
+    public static int rows = 32;
+    public static int tileScale = 1;
 
     public GameObject[] concretoTiles;
     public GameObject[] aguaTiles;
 
     public GameObject[] grassTiles;
 
-    public Char[] players;
+    public Char[] playerTypes;
+    public Text EnergyText;
+
+    Char selectedPlayer;
 
     void Awake()
     {
         MakeFloor();
+        EnergyText = GameObject.Find("EnergyText").GetComponent<Text>();
         CreatePlayers();
-    }
 
+        selectedPlayer.setEnergy();
+    }
 
     void CreatePlayers()
     {
-        for (int i = 0, len = players.Length; i < len; i++)
+        for (int i = 0, len = playerTypes.Length; i < len; i++)
         {
-
-            Instantiate(players[i], new Vector3(players[i].startX, 0, players[i].startY), Quaternion.identity);
+            var newPlayerinstanc = Instantiate(playerTypes[i], new Vector3((float)playerTypes[i].startX, (float)playerTypes[i].startY + 0.2f, 0), Quaternion.identity);
+            if (selectedPlayer == null)
+            {
+                newPlayerinstanc.energy_text = EnergyText;
+                selectedPlayer = newPlayerinstanc;
+            }
         }
-
     }
 
     void MakeFloor()
     {
         _boardHolder = new GameObject("Board").transform;
 
-        for (int x = 0; x < rows; x++)
+        for (int x = 0; x < rows; x += tileScale)
         {
-            for (int y = 0; y < columns; y++)
+            for (int y = 0; y < columns; y += tileScale)
             {
 
                 GameObject toInstantiate = grassTiles[0];
@@ -69,7 +79,7 @@ public class CenarioCreator : MonoBehaviour
                     toInstantiate = aguaTiles[0];
                 }
 
-                GameObject instance = Instantiate(toInstantiate, new Vector3(x, 0f, y), Quaternion.identity) as GameObject;
+                GameObject instance = Instantiate(toInstantiate, new Vector3Int(x, y, 0), Quaternion.identity) as GameObject;
 
                 instance.transform.SetParent(_boardHolder);
             }
@@ -79,13 +89,121 @@ public class CenarioCreator : MonoBehaviour
 
     bool InsideCircle(int[] center, int radius, int[] position)
     {
-        float[] centro = new float[] { center[0], center[1] };
-        float raio = radius;
+        int[] centro = new int[] { center[0], center[1] };
+        int raio = radius;
         return Math.Round(Math.Sqrt(Math.Pow(centro[0] - position[0], 2) + Math.Pow(centro[1] - position[1], 2))) <= raio;
     }
 
     void Update()
     {
+        ClickOperations();
+    }
 
+    void ClickOperations()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Camera.allCameras[0] == null)
+                return;
+
+            var clickedGameObject = GetClickedGameObject();
+
+            if (clickedGameObject == null)
+                return;
+
+            if (clickedGameObject.tag == "Floor")
+            {
+                _GotoClickedTile(clickedGameObject);
+            }
+            else if (clickedGameObject.tag == "Player")
+            {
+                selectedPlayer = playerTypes.First(w => w.GetComponent<GameObject>() == clickedGameObject);
+
+                DrawSiblings(selectedPlayer.position);
+            }
+        }
+    }
+
+    private void DrawSiblings(Vector3Int centerPosition)
+    {
+        //var max = selectedPlayer.GetMaximumMoviment();
+        var siblingVector3 = SiblingVector3(centerPosition);
+
+        for (int i = 0, len = _boardHolder.childCount; i < len; i++)
+        {
+            var floor = _boardHolder.GetChild(i);
+
+            //IGNORE UNWALCABLE OBJECTS
+            if (floor.GetComponent<Collider>() == null)
+                continue;
+
+            var fPos = Vector3Int.RoundToInt(floor.transform.position);
+            if (siblingVector3.Any(a => a == fPos))
+            {
+                ChangeFloorColor(floor.gameObject, new Color(0, 255, 0));
+            }
+            else
+            {
+                ChangeFloorColor(floor.gameObject, Color.white);
+            }
+        }
+    }
+
+    private void ChangeFloorColor(GameObject floor, Color color)
+    {
+        var spriteR = floor.GetComponent<SpriteRenderer>();
+        spriteR.color = color;
+    }
+
+    private bool checkOuterLimits(Vector3Int refPosition)
+    {
+        return refPosition.x >= 0 && refPosition.y >= 0 && refPosition.x <= rows && refPosition.y <= columns;
+    }
+
+    private List<Vector3Int> SiblingVector3(Vector3Int refPosition, int factor = 1)
+    {
+        // Get X positions
+        //[x][x][x]
+        //[x][o][x]
+        //[x][x][x]
+        List<Vector3Int> siblings = new List<Vector3Int>();
+
+        siblings.Add(new Vector3Int(refPosition.x + factor, refPosition.y, 0));
+        siblings.Add(new Vector3Int(refPosition.x + factor, refPosition.y + factor, 0));
+        siblings.Add(new Vector3Int(refPosition.x + factor, refPosition.y - factor, 0));
+        siblings.Add(new Vector3Int(refPosition.x - factor, refPosition.y, 0));
+        siblings.Add(new Vector3Int(refPosition.x - factor, refPosition.y + factor, 0));
+        siblings.Add(new Vector3Int(refPosition.x - factor, refPosition.y - factor, 0));
+        siblings.Add(new Vector3Int(refPosition.x, refPosition.y + factor, 0));
+        siblings.Add(new Vector3Int(refPosition.x, refPosition.y - factor, 0));
+
+        return siblings.Where(w => checkOuterLimits(w)).ToList();
+    }
+
+    GameObject GetClickedGameObject()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            return hit.transform.gameObject;
+        }
+
+        return null;
+    }
+
+
+    void _GotoClickedTile(GameObject tile)
+    {
+        var destinePosition = Vector3Int.RoundToInt(tile.transform.position);
+
+        var moved = selectedPlayer.MoveTo(destinePosition);
+
+        if (moved)
+        {
+            var floorPos = Vector3Int.RoundToInt(tile.transform.position);
+
+            DrawSiblings(floorPos);
+        }
     }
 }
